562bc59c0e44415ac620e915e41b363e11caf4ed
vaaleee <valentina.vogel@hotmail.com>
2017-04-21 11:26:32 +0200

    noch nicht skaliert

--- aufg2.py
+++ aufg2.py
@@ -7,7 +7,7 @@
 import lxml.etree as ET
 import random
 import bz2
-
+import gc
 
 
 def gettitles(infile, testfile, trainfile, k):
@@ -15,31 +15,36 @@ def gettitles(infile, testfile, trainfile, k):
 	parser = ET.iterparse(infile, events=('end',), tag='{http://www.mediawiki.org/xml/export-0.10/}title')
 
 	reservoir = []
-	counter = 0
 
-	for event, element in parser:
+	for position, item in enumerate(parser):
+		#print(position, item[1].text)
 
-		mein_tuple = (counter, element.text)
-		if counter < k:
-			reservoir.append(element.text)
+		if position < k:
+			reservoir.append(item[1].text)
 		else:
-			m = random.randint(0, counter)
+			m = random.randint(0, position)
 			if m < k:
-				reservoir[m] = element.text
-		counter += 1
-		element.clear()
-
-	return reservoir
+				trainfile.write(reservoir[m])
+				reservoir[m] = item[1].text
+				trainfile.write('\n')
+			else:
+				trainfile.write(item[1].text)
+				trainfile.write('\n')
+		item[1].clear()
 
 
 	for word in reservoir:
 		testfile.write(word)
+
+	return reservoir
+
 
 def main():
 
 	gesamtdatei = bz2.open('dewiki-latest-pages-articles.xml.bz2', mode='r', \
 		compresslevel=9, encoding=None, errors=None, newline=None)
 
+	gc.enable()
 
 	testen = open('testfile.txt', 'w')
 	trainieren = open('trainfile.txt', 'w')
@@ -51,6 +56,7 @@ def main():
 
 	testen.close()
 	trainieren.close()
+	gesamtdatei.close()
 
 
 
