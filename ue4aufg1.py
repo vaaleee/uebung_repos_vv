e4406846a759878261757b109f0e83660abcfd14
vaaleee <valentina.vogel@hotmail.com>
2017-04-24 16:39:06 +0200

    aufgabe1 fast fertig

--- ue4aufg1.py
+++ ue4aufg1.py
@@ -6,15 +6,12 @@
 
 import lxml.etree as ET
 import glob
+import operator
 
 def getfreqwords(indir, outfile):
-
-
-	all_files = glob.glob(indir +'/SAC-Jahrbuch_*_mul.xml')
-	
-
 	my_dict = {}
-	for file in all_files:
+	for file in indir:
+		# alle sätze
 		tree = ET.iterparse(file, events=('end',), tag='s')
 
 		for _, sentence in tree:
@@ -24,37 +21,36 @@ def getfreqwords(indir, outfile):
 				lemma = str(word.get('lemma'))
 				sents += '{} {}'.format(' ', lemma)
 				word.clear()
-			if len(sents.split()) > 5:
+# 			hängt jedes mal den satz an, immer plus ein wort.
+			if len(sents.split()) > 5 and sents.endswith('.'):
 				if sents in my_dict:
 					my_dict[sents] += 1
 				else:
 					my_dict[sents] = 1
+# 			Wichtig damit der Speicher nicht stirbt.
 			sentence.clear()
 
-	#sorted_dict = sorted(my_dict, key=my_dict.get, reverse=True)
+	sorted_dict = sorted(my_dict.items(), key=operator.itemgetter(1), reverse=True)
 
-	for item in my_dict:
-		if my_dict[item] > 2:
-			print(item, my_dict[item])
+	#print(sorted_dict[:20])
 
+	for sent, number in sorted_dict[:20]:
+		outfile.write(str(number) + '\t' + str(sent) + '\n')
 
+# 	Ausgabe implementieren
 	#outfile.write(sorted_dict[0:20])
 
 
-
-
-
-
-
 def main():
+	ausgabe = open('haeufigste-saetze.txt', 'w')
+	file_names = []
+	for filename in glob.glob('Text+Berg_Release_152_v01/**/SAC-Jahrbuch_????_mul.xml', recursive = True):
+		file_names.append(filename)
 
-
-	path = 'SAC'
-	ausgabe = open('haeufigste-worter.txt', 'w')
-
-	getfreqwords(path, ausgabe)
+	getfreqwords(file_names, ausgabe)
 
 	ausgabe.close()
 
 if __name__ == '__main__':
 	main()
+# http://lxml.de/api/lxml.etree.iterparse-class.html
