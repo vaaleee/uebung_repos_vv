3d185126e3a3958ea07d6c20b9569509b3825c25
vaaleee <valentina.vogel@hotmail.com>
2017-04-24 16:45:27 +0200

    aufgabe 1 fertig

--- ue4aufg1.py
+++ ue4aufg1.py
@@ -9,48 +9,47 @@ import glob
 import operator
 
 def getfreqwords(indir, outfile):
-	my_dict = {}
-	for file in indir:
-		# alle sätze
-		tree = ET.iterparse(file, events=('end',), tag='s')
+    """Gets most frequent lemma sentences out of directory with xml files"""
+    my_dict = {}
+    for file in indir:
+        # parses the sentences for each file
+        tree = ET.iterparse(file, events=('end',), tag='s')
 
-		for _, sentence in tree:
-			sents = ''
-			wc = 0
-			for word in sentence.iterfind('.//w'):
-				lemma = str(word.get('lemma'))
-				sents += '{} {}'.format(' ', lemma)
-				word.clear()
-# 			hängt jedes mal den satz an, immer plus ein wort.
-			if len(sents.split()) > 5 and sents.endswith('.'):
-				if sents in my_dict:
-					my_dict[sents] += 1
-				else:
-					my_dict[sents] = 1
-# 			Wichtig damit der Speicher nicht stirbt.
-			sentence.clear()
+        for _, sentence in tree:
+            #makes a string out of the sentences and adds them to a dict
+            sents = ''
+            wc = 0
+            for word in sentence.iterfind('.//w'):
+                lemma = str(word.get('lemma'))
+                sents += '{} {}'.format(' ', lemma)
+                word.clear()
 
-	sorted_dict = sorted(my_dict.items(), key=operator.itemgetter(1), reverse=True)
+            if len(sents.split()) > 5 and sents.endswith('.'):
+                if sents in my_dict:
+                    my_dict[sents] += 1
+                else:
+                    my_dict[sents] = 1
+            sentence.clear()
 
-	#print(sorted_dict[:20])
+    sorted_dict = sorted(my_dict.items(), key=operator.itemgetter(1), reverse=True)
 
-	for sent, number in sorted_dict[:20]:
-		outfile.write(str(number) + '\t' + str(sent) + '\n')
+    #print(sorted_dict[:20])
+# writes output into file
+    for sent, number in sorted_dict[:20]:
+        outfile.write(str(number) + '\t' + str(sent) + '\n')
 
-# 	Ausgabe implementieren
-	#outfile.write(sorted_dict[0:20])
 
 
 def main():
-	ausgabe = open('haeufigste-saetze.txt', 'w')
-	file_names = []
-	for filename in glob.glob('Text+Berg_Release_152_v01/**/SAC-Jahrbuch_????_mul.xml', recursive = True):
-		file_names.append(filename)
+    ausgabe = open('haeufigste-saetze.txt', 'w')
+    file_names = []
+    for filename in glob.glob('Text+Berg_Release_152_v01/**/SAC-Jahrbuch_????_mul.xml', recursive = True):
+        file_names.append(filename)
 
-	getfreqwords(file_names, ausgabe)
+    getfreqwords(file_names, ausgabe)
 
-	ausgabe.close()
+    ausgabe.close()
 
 if __name__ == '__main__':
-	main()
+    main()
 # http://lxml.de/api/lxml.etree.iterparse-class.html
