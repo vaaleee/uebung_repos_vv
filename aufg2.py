98edf644e9501dd35f1b0f611a6e25936d45ff2f
vaaleee <valentina.vogel@hotmail.com>
2017-04-24 22:44:56 +0200

    aufgabe 2 funktioniert, speicher steigt nicht mehr an! :D

--- aufg2.py
+++ aufg2.py
@@ -11,57 +11,65 @@ import gc
 
 
 def gettitles(infile, testfile, trainfile, k):
-	"""no usfülle"""
-	parser = ET.iterparse(infile, events=('end',), tag='{http://www.mediawiki.org/xml/export-0.10/}title')
+    """Selects k random titles from infile for testfile, adds rest to trainfile"""
 
-	reservoir = []
+    parser = ET.iterparse(infile, events=('end',), \
+        tag='{http://www.mediawiki.org/xml/export-0.10/}title')
 
-	for position, item in enumerate(parser):
-		#print(position, item[1].text)
+    #Reservoir for sample
+    reservoir = []
 
-		if position < k:
-			reservoir.append(item[1].text)
-		else:
-			m = random.randint(0, position)
-			if m < k:
-				trainfile.write(reservoir[m])
-				reservoir[m] = item[1].text
-				trainfile.write('\n')
-			else:
-				trainfile.write(item[1].text)
-				trainfile.write('\n')
-		item[1].clear()
+    for position, item in enumerate(parser):
+
+        if position < k:
+            #adds first k items to reservoir
+            reservoir.append(item[1].text)
+        else:
+            m = random.randint(0, position)
+            if m < k:
+                trainfile.write(reservoir[m])
+                reservoir[m] = item[1].text
+                trainfile.write('\n')
+            else:
+                trainfile.write(item[1].text)
+                trainfile.write('\n')
+
+        item[1].clear()
+
+        for ancestor in item[1].xpath('ancestor-or-self::*'):
+            while ancestor.getprevious() is not None:
+                del ancestor.getparent()[0]
 
 
-	for word in reservoir:
-		testfile.write(word)
+    for word in reservoir:
+        testfile.write(word)
+        testfile.write('\n')
 
-	return reservoir
+
 
 
 def main():
 
-	gesamtdatei = bz2.open('dewiki-latest-pages-articles.xml.bz2', mode='r', \
-		compresslevel=9, encoding=None, errors=None, newline=None)
+    gesamtdatei = bz2.open('dewiki-latest-pages-articles.xml.bz2', mode='r', \
+        compresslevel=9, encoding=None, errors=None, newline=None)
 
-	gc.enable()
+    gc.enable()
 
-	testen = open('testfile.txt', 'w')
-	trainieren = open('trainfile.txt', 'w')
-
-
-	print(gettitles(gesamtdatei, testen, trainieren, 20))
+    testen = open('testfile.txt', 'w')
+    trainieren = open('trainfile.txt', 'w')
 
 
-
-	testen.close()
-	trainieren.close()
-	gesamtdatei.close()
+    print(gettitles(gesamtdatei, testen, trainieren, 100))
 
 
 
+    testen.close()
+    trainieren.close()
+    gesamtdatei.close()
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
+
+#http://stackoverflow.com/questions/12160418/why-is-lxml-etree-iterparse-eating-up-all-my-memory
